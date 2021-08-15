866c0996b5ccb585e0bf280fe1eea7d1f4bb8d87
Doug Beachy <doug.beachy@outlook.com>
2021-08-14 23:25:45 -0400

    Tweak GPL license text
    Also removed the 'makedist' solution folder (the files are all still present, however).

--- dist/makedistXR5.bat
+++ dist/makedistXR5.bat
@@ -5,7 +5,7 @@
 @rem #  This program is free software: you can redistribute it and/or modify
 @rem #  it under the terms of the GNU General Public License as published by
 @rem #  the Free Software Foundation, either version 3 of the License, or
-@rem #  (at your option) any later version.
+@rem #  any later version.
 @rem #
 @rem #  This program is distributed in the hope that it will be useful,
 @rem #  but WITHOUT ANY WARRANTY; without even the implied warranty of
